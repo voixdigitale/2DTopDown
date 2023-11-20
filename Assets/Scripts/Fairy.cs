651fa883589385b3c52227b6a2e4da3f4011341c
voixdigitale <cristian@voixdigitale.com>
2023-11-20 15:50:18 +0100

    fairy

---
+++ Assets/Scripts/Fairy.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using static Unity.VisualScripting.Member;
+
+public class Fairy : MonoBehaviour
+{
+    [SerializeField] ParticleSystem _particles;
+    [SerializeField] float _tickDelay;
+    [SerializeField] GameObject _splatPrefab;
+
+    Rigidbody2D _rb;
+
+    private void Awake() {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start() {
+        StartCoroutine(FairyCoroutine());
+    }
+
+    private IEnumerator FairyCoroutine() {
+        while (true) {
+            yield return new WaitForSeconds(_tickDelay);
+            _particles.Play();
+            Instantiate(_splatPrefab, transform.position, Quaternion.Euler(new Vector3(0f, 0f, Random.Range(0, 360))));
+        }   
+    }
+}
